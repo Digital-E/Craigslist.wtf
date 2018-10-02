3ce8bc2e44727bf6456e45908eeeb6590546b83d
Samuel Bassett <samabassett@gmail.com>
2018-10-01 21:20:37 -0500

    update

--- wordpress/wp-content/themes/craigslist.wtf/content-search.php
+++ wordpress/wp-content/themes/craigslist.wtf/content-search.php
@@ -6,7 +6,7 @@ echo '
 <span class="list-item-line1">
 <span>' .  wp_ulike('get') . '</span>
 &nbsp<span class="post-date">' . get_the_date( 'M j', $before, $after, $echo ) . '</span>
-&nbsp<span><a class="the_link" href=' . get_the_permalink() . '>' . get_the_title() . '</a></span>
+&nbsp<span class="title"><a class="the_link" href=' . get_the_permalink() . '>' . get_the_title() . '</a></span>
 &nbsp<span class="post-price btn btn-primary">' . get_field('price') . '</span>
 &nbsp<span class="post-location">(' . get_field('location') . ')</span>
 <div class="author">by ' . get_field('username') .'</div></div>
